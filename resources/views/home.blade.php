ef89ec60ea20a1d98c74240fb17b2f0d8b36c2af
deao13 <dea.oktavia-2020@vokasi.unair.ac.id>
2021-09-16 16:29:30 +0700

    commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ASSALAMUALIAKUM</title>
+</head>
+<body>
+    <h1>HALO SEMUANYA</h1>
+    <h2>KITA DARI</h2>
+    <p>KELOMPOK 8</p>
+</body>
+</html>
