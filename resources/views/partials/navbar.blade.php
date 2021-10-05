85b336f14b795519cac4a0d9fccb6413a1a21daf
NadyaLovita <nadyalovita0604@gmail.com>
2021-10-05 12:09:13 +0700

    revisi login dan home 2

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link {{ ($title === "Home") ? 'active' : '' }}"  href="/home">Home</a>
+          <a class="nav-link {{ ($title === "Home") ? 'active' : '' }}"  href="/">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link {{ ($title === "Login") ? 'active' : '' }}" href="/register">Login</a>
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('home' ,[
         "title" => "Home",
     ]);
