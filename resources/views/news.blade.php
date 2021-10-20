c21949d55aa68e38aa2bfd715e156fcbb654274e
deao13 <dea.oktavia-2020@vokasi.unair.ac.id>
2021-10-20 14:59:13 +0700

    perubahan sedikit pada news

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -3,13 +3,16 @@
 @section('container')
   
 @foreach ($news as $blog_asip)
-<article class="mb-5">
+<article class="mb-5 border-bottom pb-4">
     <h2>
-        <a href="/news/{{ $blog_asip["slug"] }}" >{{ $blog_asip["title"] }}</a>
+        <a href="/news/{{ $blog_asip["slug"] }}" 
+        class="text-decoration-none">{{ $blog_asip["title"] }}</a>
     </h2>
 
     <h5>By: {{ $blog_asip["author"] }}</h5>
     <p>{{ $blog_asip["body"] }}</p>
+
+    <a href="/news/{{ $blog_asip["slug"] }}">Read more...</a>
 </article>
 @endforeach
 @endsection
